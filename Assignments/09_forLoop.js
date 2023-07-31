6bbf4e35b28f4eb5e5de4ff3399c157cc97d937d
NickThombare0413 <nickthombare@gmail.com>
2023-07-31 16:11:44 +0530

    09- for loop and while loop assignment added
    09- for loop and while loop assignment added

---
+++ Assignments/09_forLoop.js
@@ -0,0 +1,42 @@
+console.log(`-----------------Step-1------------------ (print the numbers 5 to 15)`);
+for (let index = 5; index <=15 ; index++) 
+{
+   console.log(index);
+}
+console.log(`----------------Step-2 ------------: print  number from: 50 to 40_`);
+
+for (let index = 50; index>=40 ; index--) 
+{
+   console.log(index);
+}
+console.log(`------------Step 3----------- first 15 Odd numbers`);
+
+for (let index = 1; index<=30; index=index+2) 
+{
+   console.log(index);
+}
+console.log(`------Step 4-------first 10 Even number__`);
+
+for (let index = 0; index<=20; index=index+2) 
+{
+   console.log(index);
+}
+
+console.log(`-----Step 5------------- table of 5`);
+
+for (let index = 5; index<=50; index=index+5) 
+{
+   console.log(index);
+}
+console.log(`--------Step 6-----------table of 10`);
+
+for (let index = 10; index<=100; index=index+10) 
+{
+   console.log(index);
+}
+console.log(`--------Step 7--------table of 10 in Reverse`);
+
+for (let index = 100; index>=10; index=index-10) 
+{
+   console.log(index);
+}
---
+++ Assignments/09_whileLoop.js
@@ -0,0 +1,50 @@
+console.log(`--------Step 1--------print 5 to 15`);
+var index=5;
+while (index<=15) 
+{
+    console.log(index);
+    index++;
+}
+console.log(`---------Step 2-----------reverse number 50 to 40`);
+index=50;
+while (index>=40) 
+{
+    console.log(index);
+    index--;
+}
+console.log(`-------Step 3-----first 15 Odd number__`);
+index=1;
+while (index<=30) 
+{
+    console.log(index);
+    index=index+2;
+}
+console.log(`------Step 4---------first 10 Even number`);
+index=0;
+while (index<=20) 
+{
+    console.log(index);
+    index=index+2;
+}
+console.log(`------Step 5--------- table of 5`);
+index=5;
+while (index<=50) 
+{
+    console.log(index);
+    index=index+5;
+}
+console.log(`--------Step 6------------table of 10`);
+index=10;
+while (index<=100) 
+{
+    console.log(index);
+    index=index+10;
+}
+console.log(`----------Step 7---------table of 10 in  Reverse`);
+
+index=100;
+while (index>=10) 
+{
+    console.log(index);
+    index=index-10;
+}
