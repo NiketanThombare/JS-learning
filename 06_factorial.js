e8b9a7a6bd15f00cd81a5873afe76ca53549be7a
NickThombare0413 <nickthombare@gmail.com>
2023-08-09 15:06:25 +0530

    todays session-09-08-23 storages,callback,closures,hosting
    todays session-09-08-23 storages,callback,closures,hosting.js added

---
+++ 06_factorial.js
@@ -0,0 +1,7 @@
+let result = 1; // 3 
+for (let index = 1; index <= 5; index++) {
+    result = result * index;
+}
+console.log(`Factorial of 5 is ${result}`);
+
+//Assignment:  WAP to find the factorial of numbers: 5, 9, 11, 7
