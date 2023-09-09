92232902b527c74fab8ffcc339e0ba1e9d6a79c0
NickThombare0413 <nickthombare@gmail.com>
2023-09-09 17:36:15 +0530

    todays class files added sort()- 09/09/23
    todays class files added sort()- 09/09/23

---
+++ 26_sort().js
@@ -0,0 +1,28 @@
+const arrayNames1 = ["Stew", "Jenny", "Bill", "Larry", "Vivek"];
+
+arrayNames.sort(); // Sorting in ascending order
+console.log(arrayNames1);
+arrayNames.reverse(); // Sorting in descending order
+console.log(arrayNames1);
+
+
+
+const num="23";
+ let x=Number(num);
+ console.log(typeof x);
+
+
+ const arrayNames = ["Stew", "Jenny", "Bill", "Larry", "Vivek"];
+
+ arrayNames.sort(); // Sorting in ascending order
+ console.log(arrayNames);
+ arrayNames.reverse(); // Sorting in descending order
+ console.log(arrayNames); 
+ 
+ const arrayNums = [34, 3, 5, 11, 21, 9, 42];
+ arrayNums.sort( (n1, n2) => { // Custom logic to sort array of numbers in ascending order
+     return n1>n2 ? 1 : -1;
+ } );
+ console.log(arrayNums);
+ arrayNums.reverse();
+ console.log(arrayNums);
